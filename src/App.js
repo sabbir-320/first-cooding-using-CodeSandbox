23cda21a2dda039fbe71da1dfa97b8ca27c72e23
Sabbir Ahmed <sabbir195661@gmail.com>
2020-10-14 08:04:22 +0600

    bootstrap all buttons added

--- src/App.js
+++ src/App.js
@@ -6,7 +6,30 @@ export default function App() {
     <div className="App">
       <h1>Hello CodeSandbox</h1>
       <h2>Start editing to see some magic happen!</h2>
-      <button className="btn btn-primary">Submit</button>
+      <button type="button" class="btn btn-primary">
+        Primary
+      </button>
+      <button type="button" class="btn btn-secondary">
+        Secondary
+      </button>
+      <button type="button" class="btn btn-success">
+        Success
+      </button>
+      <button type="button" class="btn btn-danger">
+        Danger
+      </button>
+      <button type="button" class="btn btn-warning">
+        Warning
+      </button>
+      <button type="button" class="btn btn-info">
+        Info
+      </button>
+      <button type="button" class="btn btn-light">
+        Light
+      </button>
+      <button type="button" class="btn btn-dark">
+        Dark
+      </button>
     </div>
   );
 }
